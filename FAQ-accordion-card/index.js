377adbc85761a8e96474e6e8024849710b0e78df
Diego <newskyforge2@hotmail.com>
2021-11-23 18:42:48 -0600

    ADD: Added accordian styles and js functionality

--- FAQ-accordion-card/index.js
+++ FAQ-accordion-card/index.js
@@ -1,1 +1,12 @@
-  
+const faqList = document.querySelector('.question-list');
+const faqItems = faqList.querySelectorAll('.question');
+
+const clickHandler = (e) => {
+  const target = e.target.closest('li');
+  if (target) {
+    faqItems.forEach((item) => item.classList.remove('selected'));
+    target.classList.add('selected');
+  }
+};
+
+faqList.addEventListener('click', clickHandler);
